6ea747ca90f7a02cfb98b5b242ae6a5b107331b5
Sylvain Payot <sylvain.payot@gmail.com>
2026-01-04 23:12:03 -0800

    ba2h

---
+++ solutions/ba2/ba2h/__main__.py
@@ -0,0 +1,16 @@
+import sys
+from bio.motifs import total_distance
+from bio.utils import read_inputs
+
+
+def main() -> None:
+    """Implement DistanceBetweenPatternAndStrings"""
+    schema = [("pattern", str), ("Dna", str)]
+    args = read_inputs(filepath=sys.argv[1], schema=schema, last_as_a_list=True)
+
+    # run algorithm
+    print(total_distance(**args))
+
+
+if __name__ == "__main__":
+    main()
